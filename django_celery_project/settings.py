6f1c78bdcc44812af7f04b448e4adabe909d45d8
Sourabh Mishra <Sourabh.Mishra@ibm.com>
2022-11-16 09:52:30 +0530

    removed key

--- django_celery_project/settings.py
+++ django_celery_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-100p4_lcu7qvmx(mfs)@78xrsc$lj%%4oh^*roqcka!o!6c1!p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
